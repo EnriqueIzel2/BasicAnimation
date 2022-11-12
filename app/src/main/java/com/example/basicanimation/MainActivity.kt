97c43838bd26df79da0e0d6c2a616c5aa65199f4
EnriqueIzel2 <eleaoizel@gmail.com>
2022-11-12 11:22:54 -0400

    feat: create button and method to handle animations

--- app/src/main/java/com/example/basicanimation/MainActivity.kt
+++ app/src/main/java/com/example/basicanimation/MainActivity.kt
@@ -2,10 +2,39 @@ package com.example.basicanimation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.airbnb.lottie.LottieAnimationView
+import com.airbnb.lottie.LottieDrawable
 
 class MainActivity : AppCompatActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_main)
+
+    val sanji = findViewById<LottieAnimationView>(R.id.sanji_anim)
+    val blogging = findViewById<LottieAnimationView>(R.id.blogging_anim)
+    val btnController = findViewById<Button>(R.id.btn_controller)
+
+    sanji.repeatCount = LottieDrawable.INFINITE
+    blogging.repeatCount = LottieDrawable.INFINITE
+
+    btnController.setOnClickListener {
+      if (btnController.text == "Start Animation") {
+        btnController.setText("Pause Animation")
+      } else {
+        btnController.setText("Start Animation")
+      }
+
+      animatorManager(sanji)
+      animatorManager(blogging)
+    }
+  }
+
+  private fun animatorManager(anim: LottieAnimationView) {
+    if (anim.isAnimating) {
+      anim.pauseAnimation()
+    } else {
+      anim.playAnimation()
+    }
   }
 }
